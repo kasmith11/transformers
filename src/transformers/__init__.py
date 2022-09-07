7359d544b2cb69860e58451d95cf3b8e45ee0044
Matthijs Hollemans <mail@hollance.com>
2022-09-07 14:00:59 +0200

    remove SpeechT5Tokenizer + put CTC back in the name

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -332,10 +332,9 @@ _import_structure = {
     "models.speecht5": [
         "SPEECHT5_PRETRAINED_CONFIG_ARCHIVE_MAP",
         "SpeechT5Config",
+        "SpeechT5CTCTokenizer",
         "SpeechT5FeatureExtractor",
         "SpeechT5Processor",
-        "SpeechT5Tokenizer",
-        "SpeechT5Tokenizer",
     ],
     "models.splinter": ["SPLINTER_PRETRAINED_CONFIG_ARCHIVE_MAP", "SplinterConfig", "SplinterTokenizer"],
     "models.squeezebert": ["SQUEEZEBERT_PRETRAINED_CONFIG_ARCHIVE_MAP", "SqueezeBertConfig", "SqueezeBertTokenizer"],
@@ -3345,9 +3344,9 @@ if TYPE_CHECKING:
     from .models.speecht5 import (
         SPEECHT5_PRETRAINED_CONFIG_ARCHIVE_MAP,
         SpeechT5Config,
+        SpeechT5CTCTokenizer,
         SpeechT5FeatureExtractor,
         SpeechT5Processor,
-        SpeechT5Tokenizer,
     )
     from .models.splinter import SPLINTER_PRETRAINED_CONFIG_ARCHIVE_MAP, SplinterConfig, SplinterTokenizer
     from .models.squeezebert import SQUEEZEBERT_PRETRAINED_CONFIG_ARCHIVE_MAP, SqueezeBertConfig, SqueezeBertTokenizer
